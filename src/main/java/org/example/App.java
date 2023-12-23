f7e93a5421822fc97430c1260659c36cc62ca71d
Fgutzy <ferdinand.gutzy@gmx.de>
2023-12-23 10:37:55 +0300

    added all files

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static int sumOfTwoNumbers(int a, int b) {
+        return a + b;
+    }
+    public static void printHalloWorld() {
+        System.out.println("Hallo World!");
+    }
+}
