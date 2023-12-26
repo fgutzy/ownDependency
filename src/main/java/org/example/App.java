f8bdb527b13618a40a3e522bf1429d64fecdc0e4
Fgutzy <ferdinand.gutzy@gmx.de>
2023-12-26 08:28:28 +0300

    added comment

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,13 @@ package org.example;
  *
  */
 public class App {
+
+    /**
+     * This method sums up two numbers and returns the result
+     * @param a
+     * @param b
+     * @return
+     */
     public static int sumOfTwoNumbers(int a, int b) {
         return a + b;
     }
